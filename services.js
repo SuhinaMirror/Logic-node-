61a3ae7a8eb1704770c63c29bdb2a59e84269628
Yrjanainen Vaino <vaino.yrjanainen@kone.com>
2016-07-15 20:56:23 +0300

    Implement route getting

--- services.js
+++ services.js
@@ -14,10 +14,65 @@ reittiopas_service =
         // params.destination_address
         // muitakin.. TODO!
         //
-        // hae reitti käyttäen params.src ja params.dest
+
+        //EKA KUTSU
+        //http://api.reittiopas.fi/hsl/prod/?request=geocode&user=Laged&pass=Oispa3Kaliaa&key=teekkarikyla
+        // -> hae osoite
+
+        //TOKA KUTSU
+        //http://api.reittiopas.fi/hsl/prod/?request=route&user=Laged&pass=Oispa3Kaliaa&from=2546445,6675512&to=2549445,6675513
+        //(Insertoi uudet koordinaatit)
+        // -> hae aika , bussi ja pysäkki (esim.)
+        var urlSource = "http://api.reittiopas.fi/hsl/prod/?request=geocode&user=Laged&pass=Oispa3Kaliaa&key=" + params.src;
+        var urlDestination = "http://api.reittiopas.fi/hsl/prod/?request=geocode&user=Laged&pass=Oispa3Kaliaa&key=teekkarikyla" + params.dest;
+
+        var source;
+        var destination;
+        
+        var request = require("request")
+
+        //Requestaa HTTP get reittioppaan sivuilta ja tallenna tiedot jonnekki
+        request({
+                    url: urlSource,
+                    json: true
+                }, function (error, response, body) {
+
+                    if (!error && response.statusCode === 200) {
+                        console.log("SOURCE: " + body[0].coords);
+                        source = body[0].coords; // Print the json response
+                    }
+                })
+        //
+        request({
+                    url: urlDestination,
+                    json: true
+                }, function (error, response, body) {
+
+                    if (!error && response.statusCode === 200) {
+                        console.log("DESTINATION: " + body[0].coords);
+                        destination = body[0].coords; // Print the json response
+                    }
+                })
+
+        var route;
+        var urlRoute = 'http://api.reittiopas.fi/hsl/prod/?request=route&user=Laged&pass=Oispa3Kaliaa&from=' + source + '&to=' + destination;
+
+        request({
+                        url: urlRoute,
+                        json: true
+                    }, function (error, response, body) {
+                        console.log("KISSE");
+                        console.log(error);
+                        console.log(response);
+                        if (!error && response.statusCode === 200) {
+                            console.log("ROUTE: " + body);
+                            route = response; // Print the json response
+                        }
+                    })
 
         fn({
-            'ttl': 10
+            'ttl': 10,
+            'route' : route
         });
     }
 };
@@ -34,5 +89,12 @@ module.exports =
         service = service_exports[parameters.id];
         // TODO: cache results here to avoid unnecessary calls to raw_call
         service.raw_call(parameters, fn);
-    };
+    }
 };
+
+reittiopas_service.raw_call({
+    src: 'Siikakuja 2',
+    dest: 'Mannerheimintie 60'
+}, function(){
+    console.log("joo")
+})
